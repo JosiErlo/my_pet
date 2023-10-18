7daaca84b4de19f09b2e6d783ae367b69b9f4932
jzambiasi <jefersonzambiasi@gmail.com>
2023-10-18 13:20:33 -0300

    alterado algumas coisa01

--- app/Controllers/CommentController.php
+++ app/Controllers/CommentController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use CodeIgniter\Controller;
 use App\Models\CommentModel;
-use App\Models\UserModel; // Importe a classe UserModel
+use App\Models\UserModel;
 
 class CommentController extends Controller
 {
@@ -14,50 +14,58 @@ class CommentController extends Controller
     public function __construct()
     {
         $this->commentModel = new CommentModel();
-        $this->userModel = new UserModel(); // Agora você pode instanciar a classe UserModel
+        $this->userModel = new UserModel();
     }
 
     public function addComment()
     {
         if ($this->request->getMethod() === 'post') {
-            $postID = $this->request->getPost('post_id');
+            $validation = \Config\Services::validation();
+            $validationRules = [
+                'content' => 'required',
+                'user_id' => 'required',
+                'post_id' => 'required',
+            ];
+
+            if (!$this->validate($validationRules)) {
+                return redirect()->to(site_url('blog/viewpost/' . $this->request->getPost('post_id')))
+                    ->with('errors', $validation->getErrors());
+            }
+
             $commentText = $this->request->getPost('content');
             $userID = $this->request->getPost('user_id');
-            $created = date('Y-m-d H:i:s');
-           
-            debug($this->request->getPost());
+            $postID = $this->request->getPost('post_id');
+
             // Verifique se o usuário com o ID especificado existe
             $user = $this->userModel->find($userID);
             if (!$user) {
                 return redirect()->to(site_url('blog/viewpost/' . $postID))->with('error', 'Usuário não encontrado.');
             }
-    
+
             // Crie um array com os dados do comentário
             $commentData = [
                 'post_id' => $postID,
                 'content' => $commentText,
                 'user_id' => $userID,
-                'created_at' => $created, // Inclua a data de criação
+                'created_at' => date('Y-m-d H:i:s'), // Inclua a data de criação
             ];
-    
+
             // Insira o comentário no banco de dados
             $this->commentModel->insert($commentData);
-    
+
             // Redirecione de volta à página de visualização da postagem
             return redirect()->to(site_url('blog/viewpost/' . $postID))->with('success', 'Comentário adicionado com sucesso.');
         }
     }
-    
-
-
-
 
     public function viewComments($postId)
     {
-        // Recupere os comentários associados a uma postagem específica
         $comments = $this->commentModel->where('post_id', $postId)->findAll();
 
-        // Carregue a visualização com os comentários
-        return view('/viewpost', ['content' => $comments]);
+        // Carregue a visualização de postagem junto com os comentários
+        $postModel = new \App\Models\PostModel();
+        $post = $postModel->find($postId);
+
+        return view('viewpost', ['post' => $post, 'comments' => $comments]);
     }
 }
--- app/Models/CommentModel.php
+++ app/Models/CommentModel.php
@@ -8,16 +8,15 @@ class CommentModel extends Model
 {
     protected $table = 'comments';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['post_id', 'content'];
+    protected $allowedFields = ['post_id', 'content', 'user_id', 'created_at'];
 
-    public function addComment($postID, $commentText,$userID)
+    public function addComment($postID, $commentText, $userID)
     {
         $data = [
             'post_id' => $postID,
             'content' => $commentText,
             'user_id' => $userID,
-          
-
+            'created_at' => date('Y-m-d H:i:s'),
         ];
 
         return $this->insert($data);
