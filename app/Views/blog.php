85576aaee357a9625dc338e1f3dcbdfbffbdb8fd
Josiele <josi_erlo@hotmail.com>
2023-11-08 21:52:15 -0300

    AE josi

--- app/Views/blog.php
+++ app/Views/blog.php
@@ -15,26 +15,29 @@
     </div>
 
     <div class="blog">
-        <h1>Meu Blog</h1>
+        <h1>My Pet Blog</h1>
     </div>
 
-    <div class="post">
-        <p>
-            Ter um animal de estimação pode trazer inúmeros benefícios
+  
+<div class="introducao">
+    <h3 style="font-size: 20px">
+        <span>Ter um animal de estimação pode trazer inúmeros benefícios
             para a nossa vida. Além de nos proporcionarem companhia e alegria,
             os pets têm um impacto positivo em nossa saúde física, emocional e social.
             Vamos explorar a importância de ter um pet e como eles podem melhorar a nossa qualidade de vida.
             Ter um pet vai além do simples ato de cuidar de um animal.
             Eles se tornam parte de nossa família e trazem inúmeros benefícios para a nossa vida.
-            Desde proporcionar companheirismo e alegria até melhorar nossa saúde física e emocional, os pets têm um papel fundamental
-            em nosso bem-estar geral.
+            Desde proporcionar companheirismo e alegria até melhorar nossa saúde física e emocional, 
+            os pets têm um papel fundamental em nosso bem-estar geral.
             Se você ainda não tem um pet, considere a possibilidade de adotar um.
             A relação especial que você irá construir com seu animal de estimação será
             recompensadora e transformadora.
             Contribua também para aumentar as experiências que
             você tem com seu animalzinho.
-        </p>
-    </div>
+        </span>
+    </h3>
+</div>
+    
     <form action="<?= site_url('blog') ?>" method="get">
     <label for="category">Categoria:</label>
     <select id="category" name="category">
--- app/Views/index.php
+++ app/Views/index.php
@@ -15,14 +15,14 @@
     <a href="/login">Criar post</a>
     <a href="/login">Entrar</a>
 
-
 </div>
+
     <div class="blog">
-        <h1>Meu Blog</h1>
+        <h1>My Pet Blog</h1>
     </div>
 
   
-
+<div class="introducao">
     <h3 style="font-size: 20px">
         <span>Ter um animal de estimação pode trazer inúmeros benefícios
             para a nossa vida. Além de nos proporcionarem companhia e alegria,
@@ -30,8 +30,8 @@
             Vamos explorar a importância de ter um pet e como eles podem melhorar a nossa qualidade de vida.
             Ter um pet vai além do simples ato de cuidar de um animal.
             Eles se tornam parte de nossa família e trazem inúmeros benefícios para a nossa vida.
-            Desde proporcionar companheirismo e alegria até melhorar nossa saúde física e emocional, os pets têm um papel fundamental
-            em nosso bem-estar geral.
+            Desde proporcionar companheirismo e alegria até melhorar nossa saúde física e emocional, 
+            os pets têm um papel fundamental em nosso bem-estar geral.
             Se você ainda não tem um pet, considere a possibilidade de adotar um.
             A relação especial que você irá construir com seu animal de estimação será
             recompensadora e transformadora.
@@ -39,7 +39,8 @@
             você tem com seu animalzinho.
         </span>
     </h3>
-  <!-- Exiba os posts dos usuários aqui -->
+</div>
+
   <?php foreach ($posts as $post) : ?>
         <h2><?= $post['title']; ?></h2>
         <p><?= $post['content']; ?></p>
