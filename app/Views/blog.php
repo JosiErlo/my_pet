288a31796d567ec339b2078772c4f773ca82bbcd
jzambiasi <jefersonzambiasi@gmail.com>
2023-11-10 01:43:00 -0300

    dia 10/11madruga02

--- app/Views/blog.php
+++ app/Views/blog.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
+
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= base_url('../../../assets/css/style.css'); ?>">
-<<<<<<< HEAD
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
     <title>My Pet Blog</title>
-=======
-
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
-
-    
->>>>>>> 573ff081406e69ab089512fbf9b78f601e120424
 </head>
 
 <body>
@@ -26,100 +23,30 @@
                 <?= session()->get('email'); ?>
             </p>
             <form action="<?= site_url('user/delete/' . session()->get('user_id')); ?>" method="post">
-                <button type="submit"
-                    onclick="return confirm('Tem certeza que deseja excluir sua conta? Essa ação é irreversível.');">Excluir
-                    Conta</button>
+                <button type="submit" onclick="return confirm('Tem certeza que deseja excluir sua conta? Essa ação é irreversível.');">Excluir Conta</button>
             </form>
-
-<<<<<<< HEAD
-=======
-    <div class="blog">
-        <h1>My Pet Blog</h1>
-    </div>
-
-  
-    <div class="introducao">
-        <img src='../../../assets/img/arara.jpg' alt="Animais início">
-
-
-    <h3 style="font-size: 20px">
-        <p>Ter um animal de estimação pode trazer inúmeros benefícios
-            para a nossa vida. Além de nos proporcionarem companhia e alegria,
-            os pets têm um impacto positivo em nossa saúde física, emocional e social.
-            Vamos explorar a importância de ter um pet e como eles podem melhorar a nossa qualidade de vida.
-            Ter um pet vai além do simples ato de cuidar de um animal.
-            Eles se tornam parte de nossa família e trazem inúmeros benefícios para a nossa vida.
-            Desde proporcionar companheirismo e alegria até melhorar nossa saúde física e emocional, 
-            os pets têm um papel fundamental em nosso bem-estar geral.
-            Se você ainda não tem um pet, considere a possibilidade de adotar um.
-            A relação especial que você irá construir com seu animal de estimação será
-            recompensadora e transformadora.
-            Contribua também para aumentar as experiências que
-            você tem com seu animalzinho.
-        </p>
-    </h3>
-</div>
-    
-    <form action="<?= site_url('blog') ?>" method="get">
-    <label for="category">Categoria:</label>
-    <select id="category" name="category">
-        <option value="all" <?= $selectedCategory === 'all' ? 'selected' : '' ?>>Todos</option>
-        <?php
-        $categorias = [
-            1 => 'Cachorro',
-            2 => 'Gato',
-            3 => 'Peixes',
-            4 => 'Pássaros',
-            5 => 'Diversos',
-        ];
-
-        foreach ($categorias as $id => $categoria) {
-            echo "<option value='$id' " . ($selectedCategory == $id ? 'selected' : '') . ">$categoria</option>";
-        }
-        ?>
-    </select>
-    <button type="submit">Filtrar</button>
-</form>
-
-
-<div class="post-list">
-    <?php foreach ($posts as $post) : ?>
-        <div class="post">
-            <h2><?= $post['title']; ?></h2> <!-- Usar $post['title'] em vez de $post->title -->
-            <p><?= substr($post['content'], 0, 50); ?>...</p> <!-- Usar $post['content'] em vez de $post->content -->
-            <a href="<?= site_url('blog/viewpost/' . $post['id']); ?>">Leia mais</a>
->>>>>>> 573ff081406e69ab089512fbf9b78f601e120424
         </div>
-
     </div>
 
- 
-    <header>
-    <div class="logo">
-      <img src="logo.png" alt="Logo">
-    </div>
-  </header>
+    <h1 class="title">My Pet Blog</h1>
 
-  <main>
-    <div class="introducao">
-        <h1 class="title">"My Pet Blog"></h1>
-            <p>Ter um animal de estimação pode trazer inúmeros benefícios
-                para a nossa vida. Além de nos proporcionarem companhia e alegria,
-                os pets têm um impacto positivo em nossa saúde física, emocional e social.
-                Vamos explorar a importância de ter um pet e como eles podem melhorar a nossa qualidade de vida.
-                Ter um pet vai além do simples ato de cuidar de um animal.
-                Eles se tornam parte de nossa família e trazem inúmeros benefícios para a nossa vida.
-                Desde proporcionar companheirismo e alegria até melhorar nossa saúde física e emocional,
-                os pets têm um papel fundamental em nosso bem-estar geral.
-                Se você ainda não tem um pet, considere a possibilidade de adotar um.
-                A relação especial que você irá construir com seu animal de estimação será
-                recompensadora e transformadora.
-                Contribua também para aumentar as experiências que
-                você tem com seu animalzinho.
-            </p>
-            <img src='../../../assets/img/arara.jpg' alt="Animais início">
     
-    </div>
+
+   
+
+    <header>
+        <div class="logo">
+            <img src="logo.png" alt="Logo">
+        </div>
+    </header>
+  
+    <main>
+        <div class="introducao">
+           
+            <p>Ter um animal de estimação pode trazer inúmeros benefícios para a nossa vida. Além de nos proporcionarem companhia e alegria, os pets têm um impacto positivo em nossa saúde física, emocional e social. Vamos explorar a importância de ter um pet e como eles podem melhorar a nossa qualidade de vida. Ter um pet vai além do simples ato de cuidar de um animal. Eles se tornam parte de nossa família e trazem inúmeros benefícios para a nossa vida. Desde proporcionar companheirismo e alegria até melhorar nossa saúde física e emocional, os pets têm um papel fundamental em nosso bem-estar geral. Se você ainda não tem um pet, considere a possibilidade de adotar um. A relação especial que você irá construir com seu animal de estimação será recompensadora e transformadora. Contribua também para aumentar as experiências que você tem com seu animalzinho.
+            </p>
+            <img src="../../../assets/img/arara.jpg" alt="Animais início">
+        </div>
     </main>
     <form action="<?= site_url('blog') ?>" method="get">
         <label for="category">Categoria:</label>
@@ -138,27 +65,19 @@
                 echo "<option value='$id' " . ($selectedCategory == $id ? 'selected' : '') . ">$categoria</option>";
             }
             ?>
+            
         </select>
         <button type="submit">Filtrar</button>
-    </form>
-
-
-    <div class="post-list">
-        <?php foreach ($posts as $post): ?>
+        <div class="post-list">
+        <?php foreach ($posts as $post) : ?>
             <div class="post">
-                <h2>
-                    <?= $post['title']; ?>
-                </h2> <!-- Usar $post['title'] em vez de $post->title -->
-                <p>
-                    <?= substr($post['content'], 0, 50); ?>...
-                </p> <!-- Usar $post['content'] em vez de $post->content -->
+                <h2><?= $post['title']; ?></h2>
+                <p><?= substr($post['content'], 0, 50); ?>...</p>
                 <a href="<?= site_url('blog/viewpost/' . $post['id']); ?>">Leia mais</a>
-
-
             </div>
         <?php endforeach; ?>
     </div>
-
+    </form>
 </body>
 
 </html>
