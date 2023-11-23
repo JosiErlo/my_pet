dd8a8e63b7e471d7cec7fbc8187199a3b47b4279
jzambiasi <jefersonzambiasi@gmail.com>
2023-11-23 02:22:56 -0300

    registerfeito

--- app/Views/register.php
+++ app/Views/register.php
@@ -6,52 +6,53 @@
     <link rel="stylesheet" href="<?= base_url('../../../assets/css/style.css'); ?>">
 </head>
 
-<body>
+<body id="registrobody">
     <!-- Exibir mensagens de erro, se houver -->
-    <?php if (session('errors')) : ?>
+    <?php if (session('errors')): ?>
         <div class="alert alert-danger">
-        <?php foreach (session('errors') as $error) : ?>
-            <?= $error ?><br>
-        <?php endforeach; ?>
+            <?php foreach (session('errors') as $error): ?>
+                <?= $error ?><br>
+            <?php endforeach; ?>
         </div>
     <?php endif; ?>
 
-    <!-- Container principal -->
+    <!-- <div id="cachorroregistro"></div> -->
+
+    <div class="conteinerregistro"> </div>
+        <img class="cachorroregistro"src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/38816/image-from-rawpixel-id-542207-jpeg.png">
+        <div class="loginCachorroRegistro">
 
 
-<div class="cachorroregistro">
-<div class="loginCachorroRegistro">
-    <!-- Container do formulário -->
-   
-        <h1>Uhull, que bom ter você conosco!</h1>
-       <br><br><br><br><br>
+            <!-- Container do formulário -->
 
-        <!-- Formulário de registro -->
-        <form method="POST" action="/register">
-            <label for="nome">Nome completo:</label>
-            <input type="text" id="nome" name="nome" required><br>
+            <h1>Uhull, que bom ter você conosco!</h1>
+            <br><br><br><br><br>
 
-            <label for="birthdate">Data de nascimento:</label>
-            <input type="date" id="birthdate" name="birthdate" required><br>
+            <!-- Formulário de registro -->
+            <form method="POST" action="/register">
+                <label for="nome">Nome completo:</label>
+                <input type="text" id="nome" name="nome" required><br>
 
-            <label for="mother_name">Nome da Mãe:</label>
-            <input type="text" name="mother_name" id="mother_name" required><br>
+                <label for="birthdate">Data de nascimento:</label>
+                <input type="date" id="birthdate" name="birthdate" required><br>
 
-            <label for="email">E-mail:</label>
-            <input type="email" id="email" name="email" required><br>
+                <label for="mother_name">Nome da Mãe:</label>
+                <input type="text" name="mother_name" id="mother_name" required><br>
 
-            <label for="password">Senha:</label>
-            <input type="password" id="password" name="password" required><br>
+                <label for="email">E-mail:</label>
+                <input type="email" id="email" name="email" required><br>
 
-            <label for="confirm_password">Confirmar Senha:</label>
-            <input type="password" id="confirm_password" name="confirm_password" required><br>
+                <label for="password">Senha:</label>
+                <input type="password" id="password" name="password" required><br>
 
-            <input type="submit" value="Registrar">
-            <a href="<?= site_url('blog'); ?>">Voltar para o blog</a>
-        </form>
+                <label for="confirm_password">Confirmar Senha:</label>
+                <input type="password" id="confirm_password" name="confirm_password" required><br>
+
+                <input type="submit" value="Registrar">
+                <a href="<?= site_url('blog'); ?>">Voltar para o blog</a>
+            </form>
         </div>
-    </div>
-    </div>
+   
 </body>
 
 </html>
